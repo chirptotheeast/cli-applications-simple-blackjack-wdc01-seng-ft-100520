8d570d7b7137e90bec377aacab9cf7cc01c7498f
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-10-01 02:44:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -75,7 +75,7 @@ def runner
   welcome 
   initial_round
 
-  until display_card_total >= 21
+  until card_total >= 21
     card_total =  hit?(card_total)
     display_card_total(display_card)
   end
