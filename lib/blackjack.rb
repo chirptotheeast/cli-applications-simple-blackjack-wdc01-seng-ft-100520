9c4eaa2ac25e293e68318728d87bc9a2ce4b6b99
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-10-01 02:27:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,11 +45,12 @@ end
 def hit?(card_total)
   # code hit? here
   prompt_user
-   if get_user_input == 'h'
+  user_input = get_user_input
+   if user_input == 'h'
      deal_card
      card_total += deal_card 
      return card_total
-    elsif get_user_input == 's' 
+    elsif user_input == 's' 
       return card_total
     else get_user
      invalid_command
