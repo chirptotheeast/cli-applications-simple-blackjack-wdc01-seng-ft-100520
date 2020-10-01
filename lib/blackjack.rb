7aa7254fda47e99820d391d3b4b2610605946cd0
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-10-01 02:36:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,6 +46,13 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   user_input = get_user_input
+  
+  if user_input != 'h' && user_input != 's'
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  
    if user_input == 'h'
      card_total += deal_card 
     elsif user_input == 's' 
